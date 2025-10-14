02a49b4a0612195e5ec5a0507c851d25b2deb38d
salmac <salmachatoui@icloud.com>
2025-10-14 17:40:24 +0200

    tp Exercice 1

--- Exercice1/comptes.sh
+++ Exercice1/comptes.sh
@@ -1,20 +1,17 @@
-#/bin /bash 
+#!/bin/bash
 
 echo "agrument donné : $1"
 
-CHEMIN="$HOME/Journal-de-bord-individuel-PPE/Exercice1/ann"
+CHEMIN="$HOME/PPE-2025/Exercice1/ann"
 
 echo "nombre de lieux en 2016 : "
-cat $CHEMIN/2016/*.ann | grep "Location" | wc -l 
+cat $CHEMIN/2016/*/*.ann | grep "Location" | wc -l
 
 
 echo "nombre de lieux en 2017 : "
-cat $CHEMIN/2017/*.ann | grep "Location" | wc -l 
+cat $CHEMIN/2017/*/*.ann| grep "Location" | wc -l
 
 
 echo "nombre de lieux en 2018 : " 
-cat $CHEMIN/2018/*.ann | grep "Location" | wc -l 
-
-
-
+cat $CHEMIN/2018/*/*.ann| grep "Location" | wc -l
 
