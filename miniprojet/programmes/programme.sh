61d7947ce487336880e83b5934aa7b97b8148a43
salmac <salmachatoui@icloud.com>
2025-10-25 21:10:35 +0200

    miniprojet

---
+++ miniprojet/programmes/programme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+fichier=$1
+nbligne=1
+
+if [ $# -ne 1 ]
+then
+    echo "il faut un argument ! "
+    exit
+fi
+
+
+while read -r line ;
+do
+HTTP=$(curl -s -o /dev/null -w '%{http_code}' "$line")
+ENCODAGE=$(curl -s -o /dev/null -w '%{content_type}' "$line")
+MOTS=$(lynx -dump -nolist "$line" |wc -w );
+
+echo -e " $nbligne \t ${line}\t $HTTP \t $ENCODAGE \t $MOTS";
+
+nbligne=$( expr $nbligne + 1);
+done < $fichier ;
